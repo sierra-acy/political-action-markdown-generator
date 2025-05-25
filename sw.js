e0a0f2dcccb792dcf776c6a0db8bc4833669f56c
Sierra Acy <sierra.acy@gmail.com>
2025-05-24 17:40:24 -0700

    set up service worker and manifest

---
+++ sw.js
@@ -0,0 +1,63 @@
+const VERSION = "v1";
+const CACHE_NAME = `markdown-gen-${VERSION}`;
+
+
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/callRepsFieldset.html",
+  "/style.css",
+  "/app.js",
+  "/markdowngen.json",
+  "/icons/united-states-flag-icon-small.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+
+self.addEventListener("fetch", (event) => {
+  // when seeking an HTML page
+  if (event.request.mode === "navigate") {
+    // Return to the index.html page
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For every other request type
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // Respond with a HTTP 404 response status.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
